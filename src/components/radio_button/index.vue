de700956182aaf013dcf5f74ee5307d304a77aea
xanadu-zxl <xanadu.zxl@gamil.com>
2020-12-28 00:26:13 +0800

    feat：单选组件样式

--- src/components/radio_button/index.vue
+++ src/components/radio_button/index.vue
@@ -1,143 +1,129 @@
 <template>
-  <van-field
-    :id="field.identity_key"
-    :label="field.title"
-    :class="[statusClass, field.customClass]"
-    :disabled="disabled"
-    :error-message="errorMessage"
-  >
-    <template #input>
-      <brick-radio-button-select
-        v-model="selectedValue"
-        :options="options"
-      >
-        <template #optionTemplate="{option}">
-          <div
-            v-if="option.other_option"
-            class="other-select-item"
+  <div class="radio">
+    <van-field
+      v-if="field.settings.layout !== 'select'"
+      :id="field.identity_key"
+      :label="field.title"
+      :class="[statusClass, field.customClass, field.settings.layout]"
+      :disabled="disabled"
+      :error-message="errorMessage"
+    >
+      <template #input>
+        <p class="description">
+          {{ field.description }}
+        </p>
+        <van-radio-group v-model="radio">
+          <van-radio
+            v-for="option in field.options"
+            :key="option.id"
+            :name="option.id"
           >
-            <div
+            {{ option.value }}
+          </van-radio>
+          <van-radio v-if="field.other_option">
+            {{ field.other_option }}
+            <input
               class="other-option"
-              @click.stop
+              type="text"
             >
-              <div class="other-option-label">
-                {{ option.name }}
-              </div>
-              <input
-                v-model="otherValue"
-                class="other-option-input"
-              >
-            </div>
-          </div>
-          <span v-else>
-            {{ option.label }}
-          </span>
-        </template>
-      </brick-radio-button-select>
-    </template>
-  </van-field>
+          </van-radio>
+        </van-radio-group>
+      </template>
+    </van-field>
+    <!-- 单选下拉组件 -->
+    <van-field
+      v-else
+      v-model="selectedValue"
+      :label="field.title"
+      :class="[statusClass, field.customClass, field.settings.layout]"
+      :disabled="disabled"
+      :error-message="errorMessage"
+      placeholder="请选择"
+      readonly
+      right-icon="arrow-down"
+      @click="show = true"
+    />
+    <van-popup
+      v-model="show"
+      position="bottom"
+      :style="{ height: '50%' }"
+      round
+    >
+      <van-cell
+        class="popup-head"
+        title="取消"
+        value="确认"
+        @click="show = false"
+      />
+      <div class="popup">
+        <van-radio-group v-model="radio">
+          <van-cell-group>
+            <van-cell
+              v-for="option in field.options"
+              :key="option.id"
+              :title="option.value"
+              clickable
+              @click="onConfirm(option, field)"
+            >
+              <template #right-icon>
+                <van-radio :name="option.id" />
+              </template>
+            </van-cell>
+          </van-cell-group>
+        </van-radio-group>
+      </div>
+    </van-popup>
+  </div>
 </template>
 
 <script>
-import _ from 'lodash'
-import { BrickRadioButtonSelect } from '@byzanteam/vis-components'
+import {
+  RadioGroup, Radio, Cell, CellGroup, Popup,
+} from 'vant'
 import FieldMixin from '../mixin'
 
 export const RadioButton = {
   mixins: [FieldMixin],
-
   components: {
-    BrickRadioButtonSelect,
+    vanRadioGroup: RadioGroup,
+    vanRadio: Radio,
+    vanCell: Cell,
+    vanCellGroup: CellGroup,
+    vanPopup: Popup,
   },
-
   data() {
     return {
-      otherValue: '',
-      selectedValue: null,
+      radio: '',
+      value: '',
+      show: false,
+      selectedValue: '',
     }
   },
 
-  computed: {
-    initalValue() {
-      const entry = this.entries.find(item => !item._destroy)
-      if (!entry) return null
-      return {
-        ...entry,
-        id: entry.option_id,
-        label: entry.value,
-      }
-    },
-
-    options() {
-      const { other_option: otherOption } = this.field
-      const options = this.field.options.map(option => ({
-        id: option.id,
-        label: option.value,
-        value: option.value,
-      }))
-      if (otherOption) {
-        const option = {
-          id: 0,
-          name: otherOption,
-          label: this.otherValue,
-          value: otherOption,
-          other_option: true,
-        }
-        return options.concat(option)
-      }
-      return options
-    },
-  },
+  computed: {},
 
   watch: {
-    initalValue: {
-      handler(value) {
-        this.selectedValue = value
-      },
-      immediate: true,
-    },
+    // initalValue: {
+    //   handler(value) {
+    //     this.selectedValue = value
+    //   },
+    //   immediate: true,
+    // },
   },
 
   methods: {
-    getEntries() {
-      const entry = _.first(this.entries)
-      const option = this.selectedValue
-      const entries = []
-
-      if (entry) {
-        if (option) {
-          if (option.id !== 0 && option.id === entry.option_id) {
-            entries.push(_.clone(entry))
-          } else {
-            entries.push(_.extend(_.clone(entry), { _destroy: true }))
-            entries.push(this._generateEntryFromOption(option))
-          }
-        } else {
-          entries.push(_.extend(_.clone(entry), { _destroy: true }))
-        }
-      } else if (option) {
-        entries.push(this._generateEntryFromOption(option))
-      }
-
-      return entries
+    onConfirm(option) {
+      this.selectedValue = option.value
+      this.radio = option.id
     },
-
-    getValid() {
-      if (!this.selectedValue && this.required) {
-        this.valid = false
-      } else {
-        this.valid = true
-      }
-      return this.valid
-    },
-
-    _generateEntryFromOption(option) {
-      return {
-        value: option.value,
-        field_id: this.field.id,
-        option_id: option.id,
-      }
-    },
+    // getValid() {
+    //   if (!this.selectedValue && this.required) {
+    //     this.valid = false
+    //   } else {
+    //     this.valid = true
+    //   }
+    //   return this.valid
+    // },
   },
 }
 

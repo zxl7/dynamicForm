90a775d9bb23d7821c2276d6c027364c44393df2
xanadu-zxl <xanadu.zxl@gamil.com>
2020-12-28 16:28:57 +0800

    feat：多选组件

--- src/components/checkbox_button/index.vue
+++ src/components/checkbox_button/index.vue
@@ -20,12 +20,12 @@
             v-for="option in field.options"
             :key="option.id"
             checked-color="#fd7d58"
-            :name="option.id"
+            :name="option"
             shape="square"
           >
             {{ option.value }}
           </van-checkbox>
-          <van-checkbox
+          <!-- <van-checkbox
             v-if="field.other_option"
             shape="square"
             checked-color="#fd7d58"
@@ -35,7 +35,7 @@
               class="other-option"
               type="text"
             >
-          </van-checkbox>
+          </van-checkbox> -->
         </van-checkbox-group>
       </template>
     </van-field>
@@ -77,7 +77,7 @@
               <template #right-icon>
                 <van-checkbox
                   ref="checkboxes"
-                  :name="option.id"
+                  :name="option"
                   checked-color="#fd7d58"
                   shape="square"
                 />
@@ -94,6 +94,7 @@
 import {
   Checkbox, CheckboxGroup, Cell, CellGroup, Popup,
 } from 'vant'
+import _ from 'lodash'
 import FieldMixin from '../mixin'
 
 export const CheckboxButton = {
@@ -114,13 +115,100 @@ export const CheckboxButton = {
       showCheck: false,
     }
   },
-  mounted() {
-
+  computed: {
+    initalValue() {
+      return this.entries.map(item => ({
+        ...item,
+        value: item.value,
+        label: item.value,
+      }))
+    },
+    options() {
+      const { other_option: otherOption } = this.field
+      const options = this.field.options.map(option => ({
+        id: option.id,
+        label: option.value,
+        value: option.value,
+      }))
+      if (otherOption) {
+        const option = {
+          id: 0,
+          name: otherOption,
+          label: this.otherValue,
+          value: otherOption,
+          other_option: true,
+        }
+        return options.concat(option)
+      }
+      return options
+    },
+  },
+  watch: {
+    initalValue: {
+      handler(value) {
+        this.selectedValue = value
+      },
+      immediate: true,
+    },
+    selectedValue: {
+      handler(value) {
+        this.checkboxValue = ''
+        value.forEach((select) => {
+          this.checkboxValue += `${select.value}、`
+        })
+      },
+      immediate: true,
+    },
   },
   methods: {
     toggle(index) {
       this.$refs.checkboxes[index].toggle()
-      this.checkboxValue = this.selectedValue.join(',')
+    },
+    getEntries() {
+      const fieldId = this.field.id
+      const oldEntries = this.entries.slice()
+      let result = []
+      this.selectedValue.forEach((option) => {
+        const oldEntryIndex = _.findIndex(oldEntries, (entry) => {
+          if (option.id) {
+            const optionId = entry.option_id ? entry.option_id.toString() : ''
+            return optionId === option.id.toString()
+          }
+          return !entry.option_id
+        })
+        if (oldEntryIndex === -1) {
+          result.push({
+            option_id: option.id,
+            value: option.value,
+            field_id: fieldId,
+          })
+        } else {
+          const oldEntry = _.clone(oldEntries.splice(oldEntryIndex, 1)[0])
+          oldEntry._destroy = false
+          if (!oldEntry.option_id) {
+            oldEntry.value = option.value
+          }
+          result.push(oldEntry)
+        }
+      })
+      const deletedOldEntries = []
+      _.each(oldEntries, (entry) => {
+        if (entry.id) {
+          const oldEntry = _.clone(entry)
+          oldEntry._destroy = true
+          deletedOldEntries.push(oldEntry)
+        }
+      })
+      result = result.concat(deletedOldEntries)
+      return result.concat(deletedOldEntries)
+    },
+    getValid() {
+      if (this.selectedValue.length <= 0 && this.required) {
+        this.valid = false
+      } else {
+        this.valid = true
+      }
+      return this.valid
     },
   },
 }

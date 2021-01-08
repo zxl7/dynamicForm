29e4cabdc83fbe1d3b667175c47e34a39ae1780a
xanadu-zxl <xanadu.zxl@gamil.com>
2021-01-08 11:54:23 +0800

    feat：缓存的图片+附件展示

--- src/components/uploader/index.vue
+++ src/components/uploader/index.vue
@@ -27,7 +27,6 @@ import FieldMixin from '../mixin'
 
 export const Upload = {
   mixins: [FieldMixin],
-
   props: {
     action: {
       type: String,
@@ -37,30 +36,55 @@ export const Upload = {
       type: Object,
       default: () => {},
     },
-
     beforeUploadFunc: {
       type: Function,
       required: true,
     },
   },
-
   components: {
     'van-uploader': Uploader,
     'i-button': Button,
     Toast,
   },
-
+  watch: {
+    initalValue: {
+      handler(value) {
+        console.log(value)
+        this.imageUrlToBase64(value)
+      },
+      immediate: true,
+    },
+  },
   data() {
     return {
       files: [],
       fileList: [],
     }
   },
+  mounted() {},
   methods: {
+    imageUrlToBase64(imageUrl) {
+      const image = new Image()
+      image.setAttribute('crossOrigin', 'anonymous')
+      image.src = imageUrl
+      image.onload = () => {
+        const canvas = document.createElement('canvas')
+        canvas.width = image.width
+        canvas.height = image.height
+        const context = canvas.getContext('2d')
+        context.drawImage(image, 0, 0, image.width, image.height)
+        const quality = 0.8
+        // base64类型
+        const dataURL = canvas.toDataURL('image/jpeg', quality)
+        // 图片的显示
+        this.fileList.push({ content: dataURL, file: 'File' })
+      }
+    },
     afterRead(file) {
       this.beforeUploadFunc(file)
     },
     getEntries() {
+      // 发给流程
       console.log(this.fileParams)
       this.files = this.fileParams.files
       if (this.files.length <= 0) return []

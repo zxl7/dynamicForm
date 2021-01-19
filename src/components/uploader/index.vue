bf6954842975ef4e94cda8862d28c058b0905cde
xanadu-zxl <xanadu.zxl@gamil.com>
2021-01-19 11:05:22 +0800

    feat：修改附件缓存 实现方式

--- src/components/uploader/index.vue
+++ src/components/uploader/index.vue
@@ -64,7 +64,7 @@ export const Upload = {
         if (dataList.length > 0) {
           dataList.forEach((res) => {
             if (this.noRepeat) {
-              this.imageUrlToBase64(res.attachment.download_url, res.attachment.name)
+              this.urlToFile(res.attachment.download_url, res.attachment.name)
             }
           })
         }
@@ -84,41 +84,31 @@ export const Upload = {
     onOversize() {
       Toast.fail(`文件大小不能超过 ${this.fileParams.size} M`)
     },
-    // 图片转base64
-    imageUrlToBase64(imageUrl, fileName) {
-      const image = new Image()
-      image.setAttribute('crossOrigin', 'anonymous')
-      image.src = imageUrl
-      image.onload = () => {
-        const canvas = document.createElement('canvas')
-        canvas.width = image.width
-        canvas.height = image.height
-        const context = canvas.getContext('2d')
-        context.drawImage(image, 0, 0, image.width, image.height)
-        const quality = 0.8
-        // base64类型
-        const dataURL = canvas.toDataURL('image/jpeg', quality)
-        // 图片的显示
-        this.fileList.push({ content: dataURL, file: 'File', name: fileName })
-        this.base64ToFile(dataURL, fileName)
-      }
-    },
-    // 转文件
-    base64ToFile(dataURL, fileName) {
-      const arr = dataURL.split(',')
-      const mime = arr[0].match(/:(.*?);/)[1]
-      const bytes = atob(arr[1]) // 解码base64
-      let n = bytes.length
-      const ia = new Uint8Array(n)
-      // eslint-disable-next-line no-plusplus
-      while (n--) {
-        ia[n] = bytes.charCodeAt(n)
-      }
-      const file = { file: new File([ia], fileName, { type: mime }) }
-      this.afterRead(file)
-      this.noRepeat = false
+
+    urlToFile(url, name) {
+      fetch(url).then(res => res.blob().then((blob) => {
+        // eslint-disable-next-line no-param-reassign
+        blob.lastModifiedDate = new Date()
+        // eslint-disable-next-line no-param-reassign
+        blob.name = name
+        const file = { file: new File([blob], blob.name, { type: blob.type }) }
+        // 图片后缀
+        if (!/\.(jpg|jpeg|png|GIF|JPG|PNG)$/.test(file.file.name)) {
+          this.fileList.push({ file: file.file, name: blob.name })
+        } else {
+          const reader = new FileReader()
+          reader.onloadend = () => {
+            const b64 = reader.result
+            this.fileList.push({ content: b64, file: file.file, name: blob.name })
+          }
+          reader.readAsDataURL(blob)
+        }
+        this.afterRead(file)
+        this.noRepeat = false
+      }))
     },
     afterRead(file) {
+      console.log(file)
       if (Array.isArray(file)) {
         file.forEach((res) => {
           // eslint-disable-next-line no-plusplus

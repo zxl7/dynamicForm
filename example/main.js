9467d5fe7b76001a1c6fab177d27cff899bd9c61
xanadu-zxl <xanadu.zxl@gamil.com>
2021-05-12 12:00:37 +0800

    fix： 删除多余注释

--- example/main.js
+++ example/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// import '@byzanteam/vis-components/dist/index.css'
-
-
 Vue.config.productionTip = false
 
 new Vue({
